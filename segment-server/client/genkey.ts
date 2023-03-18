2203df47dfdeedaca35c8252e16570f915bf09cc
Rostás Raul <raul.rostas08@gmail.com>
2023-03-18 18:33:21 +0100

    [server] fix .gitignore, delete unwanted files

--- segment-server/client/genkey.ts
+++
@@ -1,6 +0,0 @@
-import { writeFileSync } from 'fs';
-import * as rsa from 'node-rsa';
-
-const keypair = new rsa({ b: 1024 });
-writeFileSync('./private', keypair.exportKey('private'));
-writeFileSync('./public', keypair.exportKey('public'));
